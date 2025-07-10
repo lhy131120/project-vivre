79a0700e5437d995a15c3f48009caacc0d5c494a
lhy <lhy131120@gmail.com>
2025-07-11 04:17:17 +0800

    update article page

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,49 @@
 console.log("main.js run");
-document.addEventListener('DOMContentLoaded', () => {
-  const closeBtn = document.querySelector('.header .close-btn');
-  const collapse = document.querySelector('.header .collapse');
-  
-  closeBtn.addEventListener('click', () => {
-    collapse.classList.toggle('show');
-    closeBtn.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", () => {
+  const closeBtn = document.querySelector(".header .close-btn");
+  const collapse = document.querySelector(".header .collapse");
+
+  closeBtn.addEventListener("click", () => {
+    collapse.classList.toggle("show");
+    closeBtn.classList.toggle("open");
   });
 
   // Close menu when clicking outside
-  document.addEventListener('click', (e) => {
-    if (!collapse.contains(e.target) && !closeBtn.contains(e.target) && collapse.classList.contains('show')) {
-      collapse.classList.remove('show');
-      closeBtn.classList.remove('open');
+  document.addEventListener("click", (e) => {
+    if (
+      !collapse.contains(e.target) &&
+      !closeBtn.contains(e.target) &&
+      collapse.classList.contains("show")
+    ) {
+      collapse.classList.remove("show");
+      closeBtn.classList.remove("open");
     }
   });
 
   // Close menu on window resize if larger than mobile breakpoint
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 991.98 && collapse.classList.contains('show')) {
-      collapse.classList.remove('show');
-      closeBtn.classList.remove('open');
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.98 && collapse.classList.contains("show")) {
+      collapse.classList.remove("show");
+      closeBtn.classList.remove("open");
     }
+  });
+
+  // swiper
+
+  const swiper = new Swiper(".cardWrap", {
+    direction: "horizontal",
+    grid: {
+      rows:1
+    },
+    spaceBetween: 24,
+    slidePerView: 1,
+    breakpoints: {
+      992: {
+        grid: {
+          rows: 3,
+          fill: "row",
+        },
+      },
+    },
   });
 });
